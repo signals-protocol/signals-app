7c1300ab90655421ad22b1c6ca30107df317b9e6
Sooyoung Lee <dannylisa67@gmail.com>
2025-04-05 21:09:01 +0800

    resolve error

--- src/components/features/history/EndedPredictionRow.tsx
+++ src/components/features/history/EndedPredictionRow.tsx
@@ -6,7 +6,7 @@ interface EndedPredictionRowProps {
 
 export function EndedPredictionRow({ prediction }: EndedPredictionRowProps) {
   return (
-    <tr key={prediction.id}>
+    <tr>
       <td className="px-6 py-4 whitespace-nowrap">
         <div className="text-sm font-medium text-gray-900">
           {prediction.range}
